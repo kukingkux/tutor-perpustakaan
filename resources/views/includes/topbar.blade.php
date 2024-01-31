370181edc9ab156951c572906b957cc6868d8181
kukingkux <cbmamangbae@gmail.com>
2024-01-31 22:24:37 +0700

    navbar

---
+++ resources/views/includes/topbar.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
+    <div class="container my-4 justify-content-between">
+        <div class="d-flex flex-fill">
+            <a class="navbar-brand" href="#"><img src="{{asset('assets/img/logo-libtel.svg')}}" alt="logo" width="128"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav font-weight-bold">
+                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link" href="#">Katalog</a>
+                <a class="nav-link" href="#">Kontak Kami</a>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="navbar-nav font-weight-bold">
+                <a class="nav-link active" href="#">Masuk <span class="sr-only">(current)</span></a>
+                <button type="button" class="btn btn-primary px-4">Daftar</button>
+            </div>
+        </div>
+    </div>
+</nav>
